59f00e6ec40f352a1dde573c275ea4d2d83686a7
JoseK21 <josek21nunez@gmail.com>
2021-07-09 02:59:36 -0600

    WIP FOOTER

--- footer.php
+++ footer.php
@@ -4,31 +4,34 @@
 
 
 <footer class="blog-footer">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="row align-items-center">
-                    <div class="col">
-                        imagen
-                    </div>
-                    <div class="col">
-                        <h1 class="blog-title">EL TRUEQUE</h1>
-                        <p class="lead blog-description">FERIA ORGANICA CERTIFICADA.</p>
-                    </div>
+    <div class="wrapper row align-items-end justify-content-center">
+        <div class="col-4">
+            <div class="row align-items-center ">
+                <div class="col">
+                    <a class="navbar-brand logoLG" href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo_previous.png" class="img-fluid" alt="Logo"></a>
+                </div>
+                <div class="col-8 footer-text">
+                    <h5 class="footer-title">EL TRUEQUE</h5>
+                    <p class="lead blog-description">FERIA 100% ORGANICA.</p>
                 </div>
             </div>
-            <div class="col">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/facebook.svg" class="img-fluid icon" alt="facebook">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/instagram.svg" class="img-fluid icon" alt="instagram">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/whatsapp.svg" class="img-fluid icon" alt="whatsapp">
-            </div>
-            <div class="col">
-                Column
+        </div>
+        <div class="col-2" style="margin-bottom: 10px;">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/facebook.svg" class="img-fluid icon" alt="facebook">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/instagram.svg" class="img-fluid icon" alt="instagram">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/whatsapp.svg" class="img-fluid icon" alt="whatsapp">
+        </div>
+        <div class="col-3">
+            <div class="input-group mb-2">
+                <input type="text" class="form-control" placeholder="Producto" aria-label="producto" aria-describedby="basic-addon2">
+                <span class="input-group-text search" id="basic-addon2" style="background-color: #00ACE4;">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/search.svg" class="img-fluid icon" alt="search">
+                </span>
             </div>
         </div>
     </div>
 
-<!--     <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    <!--     <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
     <p><a href="#">Back to top</a></p> -->
 </footer>
 <?php wp_footer(); ?>
